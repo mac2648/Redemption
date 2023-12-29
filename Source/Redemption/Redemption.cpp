78bb879d51ca46c210ac04237b60b757bdf40d80
Marcelo vitor Toscan de Oliveira <marcelo1112@live.com>
2023-12-29 17:03:12 -0500

    created base project

---
+++ Source/Redemption/Redemption.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Redemption.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Redemption, "Redemption" );
+ 
