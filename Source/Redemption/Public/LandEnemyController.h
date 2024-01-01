6b62f01389a7a0ce56d632d0c9fbf276a08cda85
Marcelo vitor Toscan de Oliveira <marcelo1112@live.com>
2024-01-01 16:56:23 -0500

    added empty classes

---
+++ Source/Redemption/Public/LandEnemyController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RedemptionController.h"
+#include "LandEnemyController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REDEMPTION_API ALandEnemyController : public ARedemptionController
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Redemption/Public/Rat.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnemyCharacter.h"
+#include "Rat.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REDEMPTION_API ARat : public AEnemyCharacter
+{
+	GENERATED_BODY()
+	
+};
