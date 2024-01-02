7f08645ea69820c1f2368f491033bccf64536f46
Marcelo vitor Toscan de Oliveira <marcelo1112@live.com>
2024-01-01 22:52:24 -0500

    created some BBs, and rat BP_Character/BP_controller

--- Source/Redemption/Private/Rat.cpp
+++ Source/Redemption/Private/Rat.cpp
@@ -1,5 +1,10 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "Rat.h"
+#include "Components/CapsuleComponent.h"
 
+ARat::ARat()
+{
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Temporary mesh"));
+	StaticMesh->SetupAttachment(GetCapsuleComponent());
+}
--- Source/Redemption/Public/Rat.h
+++ Source/Redemption/Public/Rat.h
@@ -13,5 +13,13 @@ UCLASS()
 class REDEMPTION_API ARat : public AEnemyCharacter
 {
 	GENERATED_BODY()
+
+protected:
+	//component used as temporary because the rat meh is not a skeletal component yet
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* StaticMesh;
+
+public:
+	ARat();
 	
 };
