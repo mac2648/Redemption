02deb026bd3e6be4d5a86a126b60afdf95d9495d
Marcelo vitor Toscan de Oliveira <marcelo1112@live.com>
2024-02-01 09:35:20 -0500

    added variables to c++ class and created BP

--- Source/Redemption/Public/Bone.h
+++ Source/Redemption/Public/Bone.h
@@ -6,21 +6,32 @@
 #include "GameFramework/Actor.h"
 #include "Bone.generated.h"
 
+class UBoxComponent;
+
 UCLASS()
 class REDEMPTION_API ABone : public AActor
 {
 	GENERATED_BODY()
 	
+protected:
+	UPROPERTY(EditAnywhere)
+	UBoxComponent* BoxComp;
+
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* BoneMesh;
+
 public:	
 	// Sets default values for this actor's properties
 	ABone();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	UFUNCTION()
+	void OnBoneBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
 };
