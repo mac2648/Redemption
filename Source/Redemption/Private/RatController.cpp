34d9ad74fb2cd7d39fda6bffee9116ffbd0d75dd
mac2648 <60781347+mac2648@users.noreply.github.com>
2024-02-16 15:12:31 -0500

    Reduced speed of player and rat

--- Source/Redemption/Private/RatController.cpp
+++ Source/Redemption/Private/RatController.cpp
@@ -2,4 +2,32 @@
 
 
 #include "RatController.h"
+#include "Perception/AISenseConfig_Sight.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Rat.h"
 
+void ARatController::UpdateSight(AActor* Actor, FAIStimulus Stimulus)
+{
+	if (Actor == UGameplayStatics::GetPlayerPawn(GetWorld(), 0))
+	{
+		if (Stimulus.WasSuccessfullySensed())
+		{
+			GetBlackboardComponent()->SetValueAsObject("Player", Actor);
+			if (UCharacterMovementComponent* MoveComp = Cast<ACharacter>(GetPawn())->GetCharacterMovement())
+			{
+				MoveComp->MaxWalkSpeed = 300;
+			}
+		}
+		else
+		{
+			GetBlackboardComponent()->SetValueAsVector("LastSeenLocation", Stimulus.StimulusLocation);
+			GetBlackboardComponent()->ClearValue("Player");
+			if (UCharacterMovementComponent* MoveComp = Cast<ACharacter>(GetPawn())->GetCharacterMovement())
+			{
+				MoveComp->MaxWalkSpeed = 150;
+			}
+		}
+	}
+}
