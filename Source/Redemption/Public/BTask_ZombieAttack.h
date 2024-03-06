f73871358ca345e53916e3f7ff8a3ccd207633a9
mac2648 <60781347+mac2648@users.noreply.github.com>
2024-03-06 16:37:10 -0500

    created attack function and changed the condition for sound searching

---
+++ Source/Redemption/Public/BTask_ZombieAttack.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTask_ZombieAttack.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REDEMPTION_API UBTask_ZombieAttack : public UBTTaskNode
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(EditAnywhere)
+	FBlackboardKeySelector IsAttacking;
+
+public:
+	UBTask_ZombieAttack();
+
+protected:
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+};
