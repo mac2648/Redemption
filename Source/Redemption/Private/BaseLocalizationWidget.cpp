962a18ba5c240ce4a6f91e1945a370b799b92de8
mac2648 <60781347+mac2648@users.noreply.github.com>
2024-04-02 20:29:26 -0400

    added UI controls for Controller users

--- Source/Redemption/Private/BaseLocalizationWidget.cpp
+++ Source/Redemption/Private/BaseLocalizationWidget.cpp
@@ -9,11 +9,14 @@
 #include "LocalizationGameMode.h"
 #include "Kismet/GameplayStatics.h"
 #include "FontManager.h"
+#include "Components/Button.h"
 
 
 void UBaseLocalizationWidget::NativeConstruct()
 {
 	Super::NativeConstruct();
+
+	bIsFocusable = true;
 }
 
 UFont* UBaseLocalizationWidget::SetFontPerLanguage() const
@@ -22,4 +25,13 @@ UFont* UBaseLocalizationWidget::SetFontPerLanguage() const
 	UE_LOG(LogTemp, Warning, TEXT("%s"), *CurrentLanguage);
 
 	return Cast<ALocalizationGameMode>(UGameplayStatics::GetGameMode(GetWorld()))->GetFontManagerInstance()->GetFonts(CurrentLanguage);
+}
+
+void UBaseLocalizationWidget::SetButtonTint(UButton* Button, FSlateColor NewColor)
+{
+	FButtonStyle Style = Button->GetStyle();
+	FSlateBrush NewSlateBrush = Style.Normal;
+	NewSlateBrush.TintColor = NewColor;
+	Style.SetNormal(NewSlateBrush);
+	Button->SetStyle(Style);
 }
